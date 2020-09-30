442d32623d645f93278153c1bc596c50630c6f69
kevinmora47 <kevinmora47@gmail.com>
2020-09-29 19:33:11 -0600

    Initial commit.

---
+++ Data_Stuctures_Guide/src/Chapter_1/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package Chapter_1;
+//My First Program in Java :D
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Is there anybody out there?");
+    }
+
+}
---
+++ Data_Stuctures_Guide/src/Chapter_1/Primitive_Variables_Declarations.java
@@ -0,0 +1,14 @@
+package Chapter_1;
+public class Primitive_Variables_Declarations {
+
+    public static void main(String[] args) {
+        //Primitive Data variables initalization.
+        int num3;
+        double minimumWage = 4.50;
+        int x = 0, num1 = 0;
+        int num2 = num1;
+        
+      
+    }
+    
+}
